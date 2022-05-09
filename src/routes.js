3799bc73634670cf5a89f44d9c69a8a5740e29ee
gospel4me <tracy@gloqal.co>
2022-05-09 19:20:22 +0100

    m

--- src/routes.js
+++ src/routes.js
@@ -36,12 +36,12 @@ Coded by www.creative-tim.com
 */
 
 // Material Dashboard 2 React layouts
-import Dashboard from "layouts/dashboard";
-import Tables from "layouts/tables";
-import Billing from "layouts/billing";
-import RTL from "layouts/rtl";
-import Notifications from "layouts/notifications";
-import Profile from "layouts/profile";
+// import Dashboard from "layouts/dashboard";
+// import Tables from "layouts/tables";
+// import Billing from "layouts/billing";
+// import RTL from "layouts/rtl";
+// import Notifications from "layouts/notifications";
+// import Profile from "layouts/profile";
 import SignIn from "layouts/authentication/sign-in";
 
 // @mui icons
